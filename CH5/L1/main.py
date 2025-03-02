95adeb347bbdba26823270add8b645ffe6472183
AndreiPitt <andreideaconu288@gmail.com>
2025-03-02 17:40:32 +0200

    I did l1,l2,l3,l4,l5 of python bootdev course

---
+++ CH5/L1/main.py
@@ -0,0 +1,3 @@
+def total_xp(level, xp_to_add):
+    level = level * 100
+    return level + xp_to_add
--- main.py
+++
@@ -1,9 +0,0 @@
-from src.Controller.controller import Controller
-from src.Model.model import Model
-from src.View.view import View
-
-if __name__ == "__main__":
-    model = Model()
-    view = View()
-    controller = Controller(model, view)
-    controller.run()
